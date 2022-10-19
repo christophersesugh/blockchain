94510c7c15b7fb7bd68418c958f3a0da9f014f35
Christopher Sesugh Aondona <christohybrid185@gmail.com>
2022-10-19 17:31:33 +0100

    Hardhat simple storage

---
+++ hardhart-simple-storage/contracts/SimpleStorage.sol
@@ -0,0 +1,29 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.8;
+
+contract SimpleStorage {
+    uint256 public favNumber;
+
+    function store(uint256 _favNumber) public {
+        favNumber = _favNumber;
+    }
+
+    function retrieve() public view returns (uint256) {
+        return favNumber;
+    }
+
+    struct People {
+        string name;
+        uint256 favNum;
+    }
+
+    People[] public person;
+
+    mapping(string => uint256) public nameToFav;
+
+    function addPerson(string memory _name, uint256 _favNum) public {
+        People memory newPerson = People({name: _name, favNum: _favNum});
+        nameToFav[_name] = _favNum;
+        person.push(newPerson);
+    }
+}
