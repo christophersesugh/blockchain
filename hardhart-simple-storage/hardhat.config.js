b66a783cd6609e374f725c035cdad8244139a16b
Christopher Sesugh Aondona <christohybrid185@gmail.com>
2022-10-20 16:54:44 +0100

    Complete hardhat basics

--- hardhart-simple-storage/hardhat.config.js
+++ hardhart-simple-storage/hardhat.config.js
@@ -1,6 +1,8 @@
 require("@nomicfoundation/hardhat-toolbox");
 require("dotenv").config();
 require("@nomiclabs/hardhat-etherscan");
+require("hardhat-gas-reporter");
+require("solidity-coverage");
 require("./tasks/block-number");
 
 /** @type import('hardhat/config').HardhatUserConfig */
@@ -8,6 +10,7 @@ require("./tasks/block-number");
 const GOERLI_RPC_URL = process.env.GOERLI_RPC_URL;
 const PRIVATE_KEY = process.env.PRIVATE_KEY;
 const ETHERSCAN_API_KEY = process.env.ETHERSCAN_API_KEY;
+const COINMARKETCAP_API_KEY = process.env.COINMARKETCAP_API_KEY;
 
 module.exports = {
     defaultNetwork: "hardhat",
@@ -25,5 +28,13 @@ module.exports = {
     solidity: "0.8.8",
     etherscan: {
         apiKey: ETHERSCAN_API_KEY,
+    },
+    gasReporter: {
+        enabled: true,
+        outputFile: "gas-report.txt",
+        noColors: true,
+        currency: "USD",
+        coinmarketcap: COINMARKETCAP_API_KEY,
+        token: "MATIC",
     },
 };
